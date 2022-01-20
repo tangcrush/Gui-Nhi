44707275aad97d602314a732b319e30fb64d12ac
tangcrush <98106868+tangcrush@users.noreply.github.com>
2022-01-20 22:52:03 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -10,7 +10,7 @@ const textConfig = {
   text9: "Vì cậu đẹp try vlllll",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Khi nào đi học lại thì tớ sẽ tặng cậu kẹo :> Cậu nhận hong nếu có thì inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
 
